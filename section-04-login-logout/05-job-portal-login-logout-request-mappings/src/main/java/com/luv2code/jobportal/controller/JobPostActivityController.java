059e00bc4214a830f9549b58f9a75ad5b088034a
Chad Darby <darby@luv2code.com>
2024-05-21 00:53:51 -0400

    init

---
+++ section-04-login-logout/05-job-portal-login-logout-request-mappings/src/main/java/com/luv2code/jobportal/controller/JobPostActivityController.java
@@ -0,0 +1,37 @@
+package com.luv2code.jobportal.controller;
+
+import com.luv2code.jobportal.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JobPostActivityController {
+
+    private final UsersService usersService;
+
+    @Autowired
+    public JobPostActivityController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/dashboard")
+    public String searchJobs(Model model) {
+
+        Object currentUserProfile = usersService.getCurrentUserProfile();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            model.addAttribute("username", currentUsername);
+        }
+
+        model.addAttribute("user", currentUserProfile);
+
+        return "dashboard";
+    }
+}
