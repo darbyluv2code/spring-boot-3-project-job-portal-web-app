6e29c2e02ffdd704a6ba4a1f46f249c3d2049896
Chad Darby <darby@luv2code.com>
2024-05-21 00:19:02 -0400

    init

---
+++ section-04-login-logout/04-job-portal-login-logout-security-and-dashboard/src/main/java/com/luv2code/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.luv2code.jobportal.services;
+
+import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.repository.UsersRepository;
+import com.luv2code.jobportal.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found user"));
+        return new CustomUserDetails(user);
+    }
+}
