655c74e742463e1c6758a24398780beee9bc4fd7
Chad Darby <darby@luv2code.com>
2024-05-21 18:55:05 -0400

    init

---
+++ section-05-recruiter-profile/02-file-upload-of-recruiter-profile-image/src/main/java/com/luv2code/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.jobportal.repository;
+
+import com.luv2code.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
