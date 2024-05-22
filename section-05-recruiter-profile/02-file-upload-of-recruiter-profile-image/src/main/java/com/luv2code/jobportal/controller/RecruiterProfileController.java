65925f073d786dce3becf4280fb233fead3401af
Chad Darby <darby@luv2code.com>
2024-05-22 17:02:10 -0400

    init

--- section-05-recruiter-profile/02-file-upload-of-recruiter-profile-image/src/main/java/com/luv2code/jobportal/controller/RecruiterProfileController.java
+++ section-05-recruiter-profile/02-file-upload-of-recruiter-profile-image/src/main/java/com/luv2code/jobportal/controller/RecruiterProfileController.java
@@ -4,15 +4,20 @@ import com.luv2code.jobportal.entity.RecruiterProfile;
 import com.luv2code.jobportal.entity.Users;
 import com.luv2code.jobportal.repository.UsersRepository;
 import com.luv2code.jobportal.services.RecruiterProfileService;
+import com.luv2code.jobportal.util.FileUploadUtil;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -44,4 +49,39 @@ public class RecruiterProfileController {
 
         return "recruiter_profile";
     }
+
+    public String addNew(RecruiterProfile recruiterProfile, @RequestParam("image") MultipartFile multipartFile, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could not " + "found user"));
+            recruiterProfile.setUserId(users);
+            recruiterProfile.setUserAccountId(users.getUserId());
+        }
+        model.addAttribute("profile", recruiterProfile);
+        String fileName = "";
+        if (!multipartFile.getOriginalFilename().equals("")) {
+            fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+            recruiterProfile.setProfilePhoto(fileName);
+        }
+        RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
+
+        String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();
+        try {
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return "redirect:/dashboard/";
+    }
 }
+
+
+
+
+
+
+
+
