6fb9186d881e8b0f02d48be9a5ae4853ffffe13d
Chad Darby <darby@luv2code.com>
2025-12-26 19:09:41 -0500

    Upgrade Spring Boot 3.5.4 to 3.5.9 and remove unused javax.swing imports (#8)

--- section-11-download-candidate-resume/01-download-candidate-resume/src/main/java/com/luv2code/jobportal/config/WebSecurityConfig.java
+++ section-11-download-candidate-resume/01-download-candidate-resume/src/main/java/com/luv2code/jobportal/config/WebSecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.swing.*;
-
 @Configuration
 public class WebSecurityConfig {
 
